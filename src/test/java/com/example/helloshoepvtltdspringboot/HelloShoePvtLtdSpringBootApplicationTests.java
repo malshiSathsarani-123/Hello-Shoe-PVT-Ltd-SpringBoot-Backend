766652c5aa4bbe8797e284dc42b3bba17f548b39
malshiSathsarani-123 <malshisathsarani78@gmail.com>
2024-04-23 16:04:09 +0530

    Initial commit

---
+++ src/test/java/com/example/helloshoepvtltdspringboot/HelloShoePvtLtdSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloshoepvtltdspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloShoePvtLtdSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
